2b7667677f9968da3014d74d39a71519efb4f7e9
Thainan Matias da Silva <thainan.matias@pipedrive.com>
2021-04-24 23:35:50 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const router = express.Router();
+const emailValidator = require("deep-email-validator");
+
+const PORT = 3000;
+
+const validateEmail = async (email) => emailValidator.validate(email);
+
+router.post("/register", async (request, response, next) => {
+  const { email } = request.body;
+
+  if (!email) return response.status(400).send({ message: "Email is missing" });
+
+  const { valid, reason, validators } = await validateEmail(email);
+
+  response.status(200).send({
+    valid,
+    reason,
+    validators,
+  });
+});
+
+app.use(bodyParser.json());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Service running on port ${PORT}`);
+});
