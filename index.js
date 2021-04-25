0d0032a1d11e142364207f8021626a9c13189803
Thainan Matias da Silva <thainan.matias@pipedrive.com>
2021-04-25 15:48:38 -0300

    Add nodemon as dev dependency

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = 3000;
 const validateEmail = async (email) => emailValidator.validate(email);
 
 router.post("/register", async (request, response, next) => {
+  console.log(await require("./lib/validators/mx").getMx("pipedrive.com"));
   const { email } = request.body;
 
   if (!email) return response.status(400).send({ message: "Email is missing" });
