89630c50ec2fbe622927a01d6ede14b3175af9cf
Thainan Matias da Silva <thainan.matias@pipedrive.com>
2021-04-25 22:27:36 -0300

    Add smtp email existence validation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const router = express.Router();
 const emailValidator = require("deep-email-validator");
+const validator = require("./lib/validators");
 
 const PORT = 3000;
 
@@ -19,6 +20,28 @@ router.post("/register", async (request, response, next) => {
     valid,
     reason,
     validators,
+  });
+});
+
+router.post("/register-manual", async (request, response, next) => {
+  const { email } = request.body;
+
+  if (!email) return response.status(400).send({ message: "Email is missing" });
+
+  const [name, domain] = email.split("@");
+  const { exchange } = await validator.getBestMx(domain);
+
+  const smtpResult = await validator.verifyEmailExistence(
+    "name@example.org",
+    email,
+    exchange
+  );
+
+  response.status(200).send({
+    name,
+    domain,
+    exchange,
+    smtpResult,
   });
 });
 
